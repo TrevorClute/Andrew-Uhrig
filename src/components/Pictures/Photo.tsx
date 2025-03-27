51d93aee02336b0905a43294bc5fc736ac0b8741
casenward <casenward@gmail.com>
2025-03-27 14:37:57 -0400

    Photos fix

--- src/components/Pictures/Photo.tsx
+++ src/components/Pictures/Photo.tsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import Photo_1 from "../../assets/Photo_1.jpg";
 import Photo_2 from "../../assets/Photo_2.jpg";
 import Photo_3 from "../../assets/Photo_3.jpg";
-
+import Photo_4 from "../../assets/Photo_4.png";
+import Photo_5 from "../../assets/Photo_5.png";
+import Photo_6 from "../../assets/Photo_6.png";
+import Photo_7 from "../../assets/Photo_7.png";
+import Photo_8 from "../../assets/Photo_8.png";
+import Photo_9 from "../../assets/Photo_9.png";
 const Picture = () => {
   const [currentImage, setCurrentImage] = useState(0);
-  const images = [Photo_1, Photo_2, Photo_3];
-  const length = images.length; // Total number of photos
+  const images = [Photo_1, Photo_2, Photo_3, Photo_4, Photo_5, Photo_6, Photo_7, Photo_8, Photo_9];
+  const length = images.length;
+  const current_number = currentImage + 1;
 
   const handleLeftClick = () => {
     setCurrentImage((prev) => (prev > 0 ? prev - 1 : prev)); // Prevent going below 0
@@ -18,15 +24,16 @@ const Picture = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      {/* Container for the image and arrows */}
       <div className="relative max-w-[50%] ml-100">
         {/* Left Arrow */}
-        <div
-          onClick={handleLeftClick}
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-300 rounded-lg px-4 py-2 flex items-center justify-center opacity-75 cursor-pointer"
-        >
-          <p className="text-2xl font-bold text-black opacity-90">&larr;</p>
-        </div>
+        {currentImage > 0 && (
+          <div
+            onClick={handleLeftClick}
+            className="absolute left-0 top-[45%] transform -translate-y-1/2 bg-gray-300 rounded-lg px-4 py-2 flex items-center justify-center opacity-75 cursor-pointer"
+          >
+            <p className="text-2xl font-bold text-black opacity-90">&larr;</p>
+          </div>
+        )}
 
         {/* Image */}
         <img
@@ -36,11 +43,20 @@ const Picture = () => {
         />
 
         {/* Right Arrow */}
-        <div
-          onClick={handleRightClick}
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-300 rounded-lg px-4 py-2 flex items-center justify-center opacity-75 cursor-pointer"
-        >
-          <p className="text-2xl font-bold text-black">&rarr;</p>
+        {currentImage < length - 1 && (
+          <div
+            onClick={handleRightClick}
+            className="absolute right-0 top-[45%] transform -translate-y-1/2 bg-gray-300 rounded-lg px-4 py-2 flex items-center justify-center opacity-75 cursor-pointer"
+          >
+            <p className="text-2xl font-bold text-black">&rarr;</p>
+          </div>
+        )}
+
+        {/* Counter */}
+        <div className="text-center mt-4">
+          <p className="text-2xl font-bold text-black">
+            Photo {current_number} of {length}
+          </p>
         </div>
       </div>
     </div>
